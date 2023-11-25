e15eff633ff3b650868b188f8b242c6905260aa9
Ashton Xiong <ashtonxiong12@gmail.com>
2023-11-24 22:37:06 -0800

    Fix bottom bar navigation and stack navigation

--- App.js
+++ App.js
@@ -5,85 +5,12 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from './components/HomePage';
 import CoursePage from './components/CoursePage';
 import Notifications from './components/Notifications';
-import BottomBar from './components/BottomBar';
 import QuestionPage from './components/QuestionPage';
-import styles from './styles';
-import Ripple from 'react-native-material-ripple';
 
-
-// const Stack = createStackNavigator();
-
-// const App = () => {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator initialRouteName="Home"
-//       screenOptions={{
-//         headerStyle: {
-//           backgroundColor: '#b986ee', // Change the default background color
-//         },
-//         headerTintColor: '#fff', // Change the default text color
-//         headerTitleStyle: {
-//           fontWeight: 'bold', // Make the default text bold
-//         },
-//       }}>
-        // <Stack.Screen name="Home" component={HomePage} />
-        // <Stack.Screen name="CoursePage" component={CoursePage} />
-        // <Stack.Screen name="Notifications" component={Notifications} />
-        // <Stack.Screen name="QuestionPage" component={QuestionPage} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// };
-
-// const App = () => {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen name="Home 1" component={BottomBar} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// };
-
-// export default App;
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-// function MyTabs() {
-//   return (
-//     <Tab.Navigator>
-//       <Tab.Screen name="Courses" component={HomePage} />
-//       <Tab.Screen name="My Questions" component={Notifications} />
-//       <Tab.Screen name="Ask" component={Notifications} />
-//       <Tab.Screen name="Notifications" component={Notifications} />
-//       <Tab.Screen name="Account" component={Notifications} />
-//     </Tab.Navigator>
-//   );
-// }
-
-
-// const App = () => {
-//   return (
-//     <NavigationContainer>
-//       <MyTabs />
-//     </NavigationContainer>
-//   );
-// };
-
-// const App = () => {
-//   return (
-//     <NavigationContainer>
-//       <Tab.Navigator>
-        // <Tab.Screen name="Courses" component={HomePage} />
-        // <Tab.Screen name="My Questions" component={Notifications} />
-        // <Tab.Screen name="Ask" component={Notifications} />
-        // <Tab.Screen name="Notifications" component={Notifications} />
-        // <Tab.Screen name="Account" component={Notifications} />
-//       </Tab.Navigator>
-//     </NavigationContainer>
-//   );
-// };
 
 const MainStack = () => {
   return (
