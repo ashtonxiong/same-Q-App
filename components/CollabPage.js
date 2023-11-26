b8f9907352ec472c04f4235527d8d92c794e2646
Ashton Xiong <ashtonxiong12@gmail.com>
2023-11-25 23:31:57 -0800

    working on collaboration page

--- components/CollabPage.js
+++ components/CollabPage.js
@@ -7,15 +7,39 @@ import Icon from 'react-native-vector-icons/SimpleLineIcons';
 const CollabPage = () => {
     const navigation = useNavigation();
 
+    const numCourses = {
+      courses: ["CS147", "ENGLISH 9CE", "CS161"],
+      numQuestions: '5',
+    }
+
     const handleJoinPress = (course) => {
       navigation.navigate('CoursePage', { course });
     };  
 
   return (
     <View style={styles.NotiContainer}>
-        <View style={styles.NotificationInfo}>
-          <Icon name="people" size={'55%'} color={"#000"} style={styles.NotiIcon} />
-          <Text style={styles.NotiText}> TEST TEST</Text>
+        <View style={styles.collabHeader}>
+          <Text style={styles.courseBoxTEXT}> Collaborating </Text>
+          <Text> Collaborating on {numCourses.numQuestions} questions</Text>
+        </View>
+
+        <View style={styles.collabBox}>
+          {/* top row */}
+          <View style={styles.collabTopRow}>
+            <Icon name="people" size="20%" style={styles.collabPeopleIcon} />
+            <Text style={{ fontSize: 20, marginLeft: "5%", marginRight: "5%"}} >Expected Help at 2:15</Text>
+            <Icon style={styles.collabEarphone} name="earphones" size="20%"/>
+          </View>
+
+          {/* middle row */}
+          <View style={styles.collabTopRow}>
+            <Text>Middle</Text>
+          </View>
+
+          {/* bottom row */}
+          <View style={styles.collabTopRow}>
+            <Text>Collaborate </Text>
+          </View>
         </View>
     </View>
   );
